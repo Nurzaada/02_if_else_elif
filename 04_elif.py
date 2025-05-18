481c7200c635f5702a57dde4c9ae3508c8d0ebee
Nurzaada <Nurzaada45@example.com>
2025-05-19 01:42:58 +0600

    Коммид2

---
+++ 04_elif.py
@@ -0,0 +1,33 @@
+# is_raining = False
+
+# if is_raining == True:
+#     print('Возьми зонт')
+#     print('Погода холодная')
+# else:
+#     print('Не возьми зонт')
+#     print('Погода теплая')
+# print('Программа завершена')
+
+
+# неправильно
+# if is_raining = True
+
+## == - сравнение
+## > - больше
+## < - меньше
+## >= - больше или равно
+## <= - меньше или равно
+## != - не равно
+
+number_1 = 10
+number_2 = 20
+
+if number_1 == number_2:
+    print('Числа равны')
+elif number_1 > number_2:
+    print('Первое число больше второго')
+elif number_1 < number_2:
+    print('Первое число меньше второго')
+
+if number_1 != number_2:
+    print('Числа не равны')
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
+<<<<<<< HEAD
 # introduction-to-python
+=======
+# 02_if_else_elif
+Условия if, else, elif, логика ветвлений
+>>>>>>> 8c724aa (Добавлен код и README для 02_if_else_elif)
